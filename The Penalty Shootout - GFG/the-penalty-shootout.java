f3f15872d0aa1eadee7e8567bf147f79fc099f13
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-07-22 11:14:04 +0530

    Added solution - LeetHub

---
+++ The Penalty Shootout - GFG/the-penalty-shootout.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+            int t = Integer.parseInt(br.readLine());
+            while(t-->0)
+                {
+                    String s = br.readLine();
+                    Solution ob = new Solution();
+                    System.out.println(ob.penaltyScore(s));
+                }
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int penaltyScore(String S)
+    {
+        int penalty=0;
+       for(int i=0; i<S.length()-1; i++){
+           if(S.charAt(i)=='2' && S.charAt(i+1)=='1')  penalty++;
+           
+           
+       }
+       return penalty;
+        // Your code goes here
+    }
+}
